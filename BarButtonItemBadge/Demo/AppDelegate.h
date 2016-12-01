c9822de2105cf2dab4a5f8aee19d5a068dd78db1
Ian MacCallum <imaccallum@ufl.edu>
2016-12-01 01:48:57 -0500

    purged old

---
+++ BarButtonItemBadge/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Ian MacCallum on 12/1/16.
+//  Copyright © 2016 Tethr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BarButtonItemBadge/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Ian MacCallum on 12/1/16.
+//  Copyright © 2016 Tethr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
